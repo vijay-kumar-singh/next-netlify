36c89a769f3dc2a5e4e058cb8fd67eb95d691c34
Vijay Singh <vijaysingh.nl@gmail.com>
2024-07-09 11:08:56 +0200

    initial commit

--- app/page.jsx
+++ app/page.jsx
@@ -32,8 +32,8 @@ export default function Page() {
         <main className="flex flex-col gap-8 sm:gap-16">
             <section className="flex flex-col items-start gap-3 sm:gap-4">
                 <ContextAlert />
-                <h1 className="mb-0">Netlify Platform Starter - Next.js</h1>
-                <p className="text-lg">Get started with Next.js and Netlify in seconds.</p>
+                <h1 className="mb-0">Vijay Singh - Frontend Solution Architect</h1>
+                <p className="text-lg">Tata Consultancy Services - Copenhagen, Denmark</p>
                 <Link
                     href="https://docs.netlify.com/frameworks/next-js/overview/"
                     className="btn btn-lg btn-primary sm:btn-wide"
